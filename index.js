8a25bbb302b9bb5c794ede4d5149bc23439eb977
Ionichi <Feryandi2303@gmail.com>
2024-02-29 19:05:23 +0700

    Fix: Tambah CORS dan function parse JSONP to JSON

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true,
+    optionSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 
 const FLICKR_API_URL = 'https://api.flickr.com/services/feeds/photos_public.gne?format=json';
 
 app.get('/api/images', async (req, res) => {
     try {
-        const response = await axios.get(FLICKR_API_URL);
-        res.json(response.data);
+        const response = await axios.get(FLICKR_API_URL)
+            .then(data => {
+                function jsonFlickrFeed(data) {
+                    res.json(data);
+                }
+                eval(data.data);
+            });
     } catch (error) {
         console.error("Error fetching images: ", error);
         res.status(500).json({error: "Failed to fetch images"});
